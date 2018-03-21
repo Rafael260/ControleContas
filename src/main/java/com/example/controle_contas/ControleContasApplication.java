cfdecc9c55b00011f4022532d5394f02dec64f46
Rafael260 <rafaom260@yahoo.com.br>
2018-03-21 10:58:38 -0300

    initial commit

---
+++ src/main/java/com/example/controle_contas/ControleContasApplication.java
@@ -0,0 +1,12 @@
+package com.example.controle_contas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleContasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleContasApplication.class, args);
+	}
+}
